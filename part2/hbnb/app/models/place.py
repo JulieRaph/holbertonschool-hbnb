313aeb797280d5231c93cd3c0c34553cce1a8858
Julie Tolve <julie.tolve@gmail.com>
2025-02-24 14:15:01 +0100

    Add Place model with attributes and methods for managing reviews and amenities

---
+++ part2/hbnb/app/models/place.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""This module for the Class Place"""
+
+
+from .base import BaseModel
+
+
+class Place(BaseModel):
+    """To create attibutes for the Class"""
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not value:
+            raise ValueError("The title is required")
+        if len(value) > 100:
+            raise ValueError("The title is too long")
+        self._title = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            raise TypeError("IThe description is invalid")
+        self._description = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError("The price is not valid")
+        if value < 0:
+            raise ValueError("The price is not valid")
+        if not value:
+            raise ValueError("The price is required")
+        self._price = value
+
+    @property
+    def latitude(self):
+        return self._latitude
+
+    @latitude.setter
+    def latitude(self, value):
+        if not isinstance(value, float):
+            raise TypeError("The latitude is not valid")
+        if value < -90 or value > 90:
+            raise ValueError("The latitude must be between -90 and 90")
+        self._latitude = value
+
+    @property
+    def longitude(self):
+        return self._longitude
+
+    @longitude.setter
+    def longitude(self, value):
+        if not isinstance(value, float):
+            raise TypeError("The longitude is not valid")
+        if value < -180 or value > 180:
+            raise ValueError("The longitude must be between -180 and 180")
+        self._longitude = value
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, value):
+        if not value:
+            raise ValueError("Owner is required")
+        self._owner = value
