4f83d1f35efdb88ff61e031ed3db5ac92d46c7f4
GabrielCoh <9924@holbertonstudents.com>
2025-02-25 12:48:09 +0100

    added the place method in the facade

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,13 +1,14 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
 from app.models.amenity import Amenity
+from app.models.place import Place
 
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        # self.place_repo = InMemoryRepository()
-        # self.review_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
     # User method
@@ -51,7 +52,22 @@ class HBnBFacade:
         return self.amenity_repo.get(amenity_id)
         pass
 
-    # Placeholder method for fetching a place by ID
-    # def get_place(self, place_id):
-    #     # Logic will be implemented in later tasks
-    #     pass
+    # Place method
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+        pass
+
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+        pass
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+        pass
+
+    def update_place(self, place_id, place_data):
+        self.place_repo.update(place_id, place_data)
+        return self.place_repo.get(place_id)
+        pass
