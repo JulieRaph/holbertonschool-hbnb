5812ecaca48a34a2c3935a56547c1a1813955a04
hmeyd <yyahmedsalm@gmail.com>
2025-02-28 13:18:19 +0100

    testing

--- part2/hbnb/testing.py
+++ part2/hbnb/testing.py
@@ -8,7 +8,7 @@ import uuid
 class TestUserEndpoints(unittest.TestCase):
 
     def setUp(self):
-        """Set up the test client before each test"""
+    
         self.app = create_app()
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
@@ -21,11 +21,25 @@ class TestUserEndpoints(unittest.TestCase):
             "email": unique_email
         })
 
-        print("Création de l'utilisateur test avec email:", unique_email)
+
         print("Status code:", response.status_code)
 
         self.assertEqual(response.status_code, 201)
         self.owner = response.json
+
+
+        unique_email = f"test.user.{uuid.uuid4()}@example.com"
+        response = self.client.post('/api/v1/users/', json={
+            "first_name": "ahmed",
+            "last_name": "User",
+            "email": unique_email
+        })
+
+
+        print("Status code:", response.status_code)
+
+        self.assertEqual(response.status_code, 201)
+        self.user = response.json
 
         # Création d'un lieu test pour les reviews
         response_place = self.client.post('/api/v1/places/', json={
@@ -43,9 +57,9 @@ class TestUserEndpoints(unittest.TestCase):
 
         self.assertEqual(response_place.status_code, 201)  # Vérifie la création du lieu
         self.place = response_place.json
-
+#testing user
     def test_create_user(self):
-        """Test user creation with valid data"""
+    
         print("Test de création d'utilisateur en cours...")
         unique_email = f"jane.doe.{uuid.uuid4()}@example.com"
         response = self.client.post('/api/v1/users/', json={
@@ -53,45 +67,75 @@ class TestUserEndpoints(unittest.TestCase):
             "last_name": "Doe",
             "email": unique_email
         })
-        print("Status code:", response.status_code)
         self.assertEqual(response.status_code, 201)
 
     def test_create_user_invalid_data(self):
-        """Test user creation with invalid data"""
+       
         response = self.client.post('/api/v1/users/', json={
             "first_name": "",
             "last_name": "",
             "email": "invalid-email"
         })
-        print("Status code:", response.status_code)
         self.assertEqual(response.status_code, 400)
 
     def test_get_users(self):
-        """Test retrieving all users"""
+       
         response = self.client.get('/api/v1/users/')
-        print("Status code:", response.status_code)
+
         self.assertEqual(response.status_code, 200)
 
+    def test_update_users(self):
+       response = self.client.put(f'/api/v1/users/{self.owner["id"]}', json={
+        "first_name": "Jane",
+        "last_name": "Goe",
+        "email": self.owner["email"]
+            })
+       self.assertEqual(response.status_code, 201)
+
+    def test_update_invalide_users(self):
+       response = self.client.put(f'/api/v1/users/{self.owner["id"]}', json={
+        "first_name": "Jane",
+        "last_name": "Goe",
+        "email": self.user["email"]
+            })
+       self.assertEqual(response.status_code, 400)
+#testing Place
+    def test_create_place(self):
+        
+        response = self.client.post('/api/v1/places/', json={
+            "title": "Studio",
+            "description": "good studio",
+            "price": 100,
+            "latitude": 37.7749,
+            "longitude": -30.4194,
+            "owner_id": self.owner["id"],
+            "amenities": []
+        })
+
+        self.assertEqual(response.status_code, 201)
+
+
+
     def test_create_place_invalid_data(self):
-        """Test de création d'un lieu avec des données invalides"""
+        
         response = self.client.post('/api/v1/places/', json={
             "title": "",
-            "description": "Invalid place",
+            "description": "joulie sudio",
             "price": "",
             "latitude": 37.7749,
             "longitude": -700.4194,  # Valeur hors plage
             "owner_id": self.owner["id"],
             "amenities": []
         })
-        print("Status code:", response.status_code)
+
         self.assertEqual(response.status_code, 400)
 
     def test_get_places(self):
-        """Test retrieving all places"""
+    
         response = self.client.get('/api/v1/places/')
-        print("Status code:", response.status_code)
-        self.assertEqual(response.status_code, 200)
 
+        self.assertEqual(response.status_code, 200)
+#teting review
     def test_create_invalide_review(self):
         response = self.client.post('/api/v1/reviews/', json={
             "place_id": self.place["id"],
@@ -99,19 +143,42 @@ class TestUserEndpoints(unittest.TestCase):
             "rating": 7,
             "text": "Super endroit!"
             })
-        print("Status code:", response.status_code)
+
         self.assertEqual(response.status_code, 400)
 
 
 
-    def test_create_invalide_aminity(self):
-        response = self.client.post('/api/v1/aminities/', json={
-            "name": "olsc,pùmw;ùd:QS%M^;zd;md:plodoldl"
+    def test_create_review(self):
+        response = self.client.post('/api/v1/reviews/', json={
+            "place_id": self.place["id"],
+            "user_id": self.user["id"],
+            "rating": 5,
+            "text": "Super endroit!"
+            })
+
+        self.assertEqual(response.status_code, 201)
+
+#test amenity
+    def test_create_invalide_amenity(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": ""
         })
-        print("Status code:", response.status_code)
-        self.assertEqual(response.status_code, 404)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_amenity(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": "picine"
+        })
+
+        self.assertEqual(response.status_code, 201)
 
 
+    def test_create_amenity(self):
+        response = self.client.post('/api/v1/amenities/', json={
+            "name": "nfeinof,eo,oifjof,oieo,if,o,fe,iijrnfnfnnufnfuueoeooirijnfzpppzjeo"
+        })
 
+        self.assertEqual(response.status_code, 400)
 if __name__ == '__main__':
     unittest.main()
