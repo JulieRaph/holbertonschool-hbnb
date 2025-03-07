d48b1c59a14563520157fb148cfaaaa9127aa62e
Pablo Nudel <pablonudel@gamil.com>
2025-03-07 15:38:41 +0100

    fix: update authentication namespace path by removing trailing slash

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -19,5 +19,5 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(auth_ns, path='/api/v1/auth/')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
