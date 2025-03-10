59acc3d0680d42f3538c128b39256e10afbb086e
Pablo Nudel <pablonudel@gamil.com>
2025-03-10 10:15:22 +0100

    feat: implement SQLAlchemyRepository for database operations

--- part3/hbnb/app/persistence/repository.py
+++ part3/hbnb/app/persistence/repository.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-# from app import db
+from app import db
 # from app.models import User, Place, Review, Amenity
 
 class Repository(ABC):
@@ -28,61 +28,64 @@ class Repository(ABC):
         pass
 
 
-# class SQLAlchemyRepository(Repository):
-#     def __init__(self, model):
-#         self.model = model
-
-#     def add(self, obj):
-#         db.session.add(obj)
-#         db.session.commit()
-
-#     def get(self, obj_id):
-#         return self.model.query.get(obj_id)
-
-#     def get_all(self):
-#         return self.model.query.all()
-
-#     def update(self, obj_id, data):
-#         obj = self.get(obj_id)
-#         if obj:
-#             for key, value in data.items():
-#                 setattr(obj, key, value)
-#             db.session.commit()
-
-#     def delete(self, obj_id):
-#         obj = self.get(obj_id)
-#         if obj:
-#             db.session.delete(obj)
-#             db.session.commit()
-
-#     def get_by_attribute(self, attr_name, attr_value):
-#         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
-
-
-class InMemoryRepository(Repository):
-    def __init__(self):
-        self._storage = {}
+class SQLAlchemyRepository(Repository):
+    def __init__(self, model):
+        self.model = model
 
     def add(self, obj):
-        self._storage[obj.id] = obj
+        db.session.add(obj)
+        db.session.commit()
 
     def get(self, obj_id):
-        return self._storage.get(obj_id)
+        return self.model.query.get(obj_id)
 
     def get_all(self):
-        return list(self._storage.values())
+        return self.model.query.all()
 
     def update(self, obj_id, data):
         obj = self.get(obj_id)
         if obj:
-            obj.update(data)
+            for key, value in data.items():
+                setattr(obj, key, value)
+            db.session.commit()
 
     def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
+        obj = self.get(obj_id)
+        if obj:
+            db.session.delete(obj)
+            db.session.commit()
 
     def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+        return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
     
     def get_all_by_attribute(self, attr_name, attr_value):
-        return [obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value]
+        return [obj for obj in self.model.query.values() if getattr(obj, attr_name) == attr_value]
+
+
+# class InMemoryRepository(Repository):
+#     def __init__(self):
+#         self._storage = {}
+
+#     def add(self, obj):
+#         self._storage[obj.id] = obj
+
+#     def get(self, obj_id):
+#         return self._storage.get(obj_id)
+
+#     def get_all(self):
+#         return list(self._storage.values())
+
+#     def update(self, obj_id, data):
+#         obj = self.get(obj_id)
+#         if obj:
+#             obj.update(data)
+
+#     def delete(self, obj_id):
+#         if obj_id in self._storage:
+#             del self._storage[obj_id]
+
+#     def get_by_attribute(self, attr_name, attr_value):
+#         return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
+    
+#     def get_all_by_attribute(self, attr_name, attr_value):
+#         return [obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value]
