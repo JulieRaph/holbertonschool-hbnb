4e2a26800a3404946c7b9095bc621a9ef832b5d2
JulieRaph <julie.tolve@gmail.com>
2025-03-11 15:59:57 +0100

    feat: integrate UserRepository and hash password during user creation

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 # from app.persistence.repository import InMemoryRepository
 from app.persistence.repository import SQLAlchemyRepository
+from app.services.repositories.user_repository import UserRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -9,13 +10,14 @@ from app.models.review import Review
 class HBnBFacade:
     def __init__(self):
         self.user_repo = SQLAlchemyRepository(User)
-        self.place_repo = SQLAlchemyRepository(Place)
-        self.review_repo = SQLAlchemyRepository(Review)
-        self.amenity_repo = SQLAlchemyRepository(Amenity)
+        # self.place_repo = SQLAlchemyRepository(Place)
+        # self.review_repo = SQLAlchemyRepository(Review)
+        # self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     # User method
     def create_user(self, user_data):
         user = User(**user_data)
+        user.hash_password(user_data['password'])
         self.user_repo.add(user)
         return user
 
