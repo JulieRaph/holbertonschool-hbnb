92c4b85053ea63f32e7aed615f7402f0f42c95c5
JulieRaph <julie.tolve@gmail.com>
2025-03-11 15:59:25 +0100

    feat: initialize Flask extensions directly in app module for improved structure

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+db = SQLAlchemy()
+
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.admin import api as admin_ns
-from app.services.extensions import bcrypt, jwt, db
-
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    bcrypt.init_app(app)
-    jwt.init_app(app)
-    db.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     
     api.add_namespace(users_ns, path='/api/v1/users')
@@ -23,4 +26,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(admin_ns, path='/api/v1/admin')
+
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     return app
