974154e33e6a68b6abc4a1c4ef3cd7a4eabc3040
Pablo Nudel <pablonudel@gamil.com>
2025-03-12 19:30:22 +0100

    refactor: remove commented-out code in Place model for cleaner implementation

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -55,9 +55,6 @@ class Place(BaseModel):
             "price": self.price,
             "latitude": self.latitude,
             "longitude": self.longitude,
-            # "owner_id": self.owner_id,
-            # "amenities": self.amenities,
-            # "reviews": self.reviews
         }
 
     @validates('title')
