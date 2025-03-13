202762d3b5ebe424a57c45d95e39fe27901f0a93
hmeyd <yyahmedsalm@example.com>
2025-03-13 13:39:54 +0100

    modif

--- part3/sql_script.sql
+++ part3/sql_script.sql
@@ -6,8 +6,8 @@ USE hbnb_db;
 -- create table User
 CREATE TABLE User (
     id CHAR(36) PRIMARY KEY NOT NULL DEFAULT (UUID()),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
