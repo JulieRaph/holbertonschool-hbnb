91b9fdea6303765b8e00f4507e6ec7970a2a2f4d
Pablo Nudel <pablonudel@gamil.com>
2025-03-15 15:07:59 +0100

    feat: enhance user input validation in UserList to enforce allowed fields

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -17,10 +17,10 @@ class UserList(Resource):
         user_data = api.payload
         email = user_data.get('email').lower()
 
-        # valid_inputs = ['first_name', 'last_name', 'email', 'password']
-        # for key in user_data:
-        #     if key not in valid_inputs:
-        #         api.abort(400, f'Invalid input data: {key}')
+        valid_inputs = ['first_name', 'last_name', 'email', 'password']
+        for key in user_data:
+            if key not in valid_inputs:
+                api.abort(400, f'Invalid input data: {key}')
         
         existing_user = facade.get_user_by_email(email)
         if existing_user:
